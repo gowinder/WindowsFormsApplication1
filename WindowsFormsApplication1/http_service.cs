6e427dfdd95f90a472b6ece6c1081830fcab9777
gowinder <gowinder@hotmail.com>
2016-03-28 21:01:56 +0800

    test

--- WindowsFormsApplication1/http_service.cs
+++ WindowsFormsApplication1/http_service.cs
@@ -5,42 +5,58 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WindowsFormsApplication1
 {
     class http_service
     {
+
         public http_service()
         {
             _listener = new HttpListener();
         }
         protected HttpListener _listener;
-    /*    public Task run()
-        {
-            return Task.Run(async () =>
+        /*    public Task run()
             {
-                try
+                return Task.Run(async () =>
                 {
-                    _listener.Start();
-                }
-                catch (HttpListenerException hlex)
-                {
-
-                    return;
-                }
-
-                while (true)
-                {
-                    _listener.GetContextAsync().ContinueWith(async (t) =>
+                    try
                     {
-                        var ctx = await t;
-                        await
-                    });
-                }
-            });
+                        _listener.Start();
+                    }
+                    catch (HttpListenerException hlex)
+                    {
+
+                        return;
+                    }
+
+                    while (true)
+                    {
+                        _listener.GetContextAsync().ContinueWith(async (t) =>
+                        {
+                            var ctx = await t;
+                            await
+                        });
+                    }
+                });
+            }
+            */
+
+        public async static Task<string> get_respons(string req)
+        {
+            var clientHandler = new HttpClientHandler();
+            var client = new HttpClient(clientHandler);
+            Console.WriteLine("begin request: {0}, task:{1}, thread{2}, ispoos{3}", req, Task.CurrentId, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            var response = client.GetAsync(req);
+            string str_respons = await response.Result.Content.ReadAsStringAsync();
+
+            Console.WriteLine("recive respons({0})", req);
+
+            return str_respons;
         }
-        */
+
 
         public void test()
         {
@@ -54,19 +70,28 @@ namespace WindowsFormsApplication1
             };
             foreach(var req in reqs)
             {
-               
+                //    Task t = Task.Run(() => get_respons_async(req));
+                //                 Task t = new Task(this.get_respons_async, req, TaskCreationOptions.LongRunning);
+                //                 t.Start();
+
+                var t1 = Task<string>.Factory.StartNew(() => get_respons(req));
+                t1.Start();
+                Console.WriteLine("return req:{0}", req);// t1.Result);
             }
         }
+
+      
 
         protected async void get_respons_async(string req)
         {
             var clientHandler = new HttpClientHandler();
             var client = new HttpClient(clientHandler);
-            Console.WriteLine("begin request: {0}", req);
+            Console.WriteLine("begin request: {0}, task:{1}, thread{2}, ispoos{3}", req, Task.CurrentId, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
             var response = await client.GetAsync(req);
             string str_respons = await response.Content.ReadAsStringAsync();
 
             Console.WriteLine("recive respons({0})", req);
         }
     }
+
 }
