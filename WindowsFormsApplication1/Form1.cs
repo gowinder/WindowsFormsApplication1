551104d0337208932fbbc4ac142348b1dbb3e216
gowinder <gowinder@hotmail.com>
2016-04-06 14:55:16 +0800

    test db

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -1,4 +1,7 @@
-﻿using NLog;
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
 using ProtoBuf;
 using System;
 using System.Collections.Generic;
@@ -9,6 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 using WindowsFormsApplication1.data;
 
@@ -38,7 +42,21 @@ namespace WindowsFormsApplication1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //    data_login dl = new data_login() { user_name = "test1", user_pwd = "1234", action_type = 1 };
+            data_login dl = new data_login() { user_name = "test1", user_pwd = "1234", action_type = 1 };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var json = serializer.Serialize(dl);
+            Console.WriteLine(json);
+
+            string str_json = @"{""user_name"":""test1"",""user_pwd"":""1234"",""action_type"":1,""ret"":0, ""fuck"":""asdf""}";
+            JObject jo = (JObject)JsonConvert.DeserializeObject(str_json);
+            string zone = jo["fuck"].ToString();
+
+
+            var p1 = serializer.Deserialize<data_login>(str_json);
+            Console.WriteLine(p1.ToString());
+
+            /*
 
             data_login dl = new data_login() { user_name = "test1", user_pwd = "1234", action_type = 1 };
 
@@ -52,6 +70,27 @@ namespace WindowsFormsApplication1
                 Console.WriteLine(ddl.ToString());
             }
             Console.WriteLine(hrd.ToString());
+            */
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var conn = new MySqlConnection("server=localhost;User Id=root;password=asdf;Database=pd");
+            conn.Open();
+            var command = new MySqlCommand("select * from game_role where id = 1", conn);
+            var rdr = command.ExecuteReader();
+            while (rdr.Read())
+            {
+                for(int i = 0; i < rdr.FieldCount; i++)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(rdr.GetName(i));
+                    sb.Append(":");
+                    sb.Append(rdr[i]);
+                    Console.WriteLine(sb.ToString());
+                }
+            }
+            rdr.Close();
         }
     }
 }
---
+++ database/i_database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database
+{
+    public interface i_database
+    {
+        bool open(string host, string db_name, string db_pwd, int port);
+
+
+    }
+}
